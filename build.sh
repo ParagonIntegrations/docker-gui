d1a41127c10e99614d2f8625bf178ef4a6774d59
Nelis Volschenk <nelisvolschenk@gmail.com>
2023-08-11 12:38:08 +0200

    change to be a base image

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-caddydockerfile = $parent_path/caddy/Dockerfile
-vncdockerfile = $parent_path/vnc_docker/Dockerfile
+caddypath=$parent_path/caddy
+vncdockerpath=$parent_path/vnc_docker
+base=python
+version=3.11.4-bookworm
+baseimg=$base:$version
+platforms=linux/amd64,linux/arm/v7,linux/arm64/v8
+notusedplatforms=linux/386
 
-docker login
-cd "$parent_path/caddy"
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag nelisvolschenk/vnc_docker_caddy:latest --push .
+docker login -u nelisvolschenk -p dckr_pat_tjT__eHx0_KRTSMOxwKFs5cKWnw
+#cd "$caddypath"
+#docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag nelisvolschenk/vnc_docker_caddy:latest --push .
 
-cd "$parent_path/vnc_docker"
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag nelisvolschenk/vnc_docker_app:latest --push .
+cd "$vncdockerpath"
+docker buildx build --build-arg BASE_IMAGE="$baseimg" --platform "$platforms" --tag "nelisvolschenk/vnc_docker_base:$base-$version" --push .
